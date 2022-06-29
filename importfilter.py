5570aa6adae71d87c2bd552378e1283b9c3da834
CorbinianGroen <corbinian.groen@web.de>
2022-06-29 14:31:35 +0200

    Change Ar to ctk boxes

--- importfilter.py
+++ importfilter.py
@@ -15,7 +15,8 @@ def importwindow(master, widthfactor, heightfactor):
     window = ct.CTkToplevel(master)
     #window.grab_set()
     window.title('Import Options')
-    window.geometry(str(int(widthfactor*750)) + 'x' + str(int(heightfactor*600)))
+    window.geometry(str(int(widthfactor*1200)) + 'x' + str(int(heightfactor*600)))
+    window.resizable(False, False)
 
     window.grid_rowconfigure(0, minsize=10)
     window.grid_rowconfigure(1, weight=5)
@@ -30,11 +31,11 @@ def importwindow(master, widthfactor, heightfactor):
     InputFrame = ct.CTkFrame(master=window, corner_radius=10, fg_color=('grey80', 'grey20'))
     InputFrame.grid(row=1, column=1, sticky='nswe')
 
-    InputFrame.grid_rowconfigure(0, weight=1)
+    InputFrame.grid_rowconfigure(0, weight=1, minsize=100)
     InputFrame.grid_columnconfigure(0, weight=1)
-    InputFrame.grid_rowconfigure(1, weight=1)
-    InputFrame.grid_rowconfigure(2, weight=0)
-    InputFrame.grid_rowconfigure(3, weight=0)
+    InputFrame.grid_rowconfigure(1, weight=1, minsize=100)
+    InputFrame.grid_rowconfigure(2, weight=1, minsize=150)
+    InputFrame.grid_rowconfigure(3, weight=1, minsize=150)
     InputFrame.grid_rowconfigure(4, minsize=10)
 
     ArFrame = ct.CTkFrame(master=InputFrame, corner_radius=10, fg_color=('grey90', 'grey10'))
@@ -47,71 +48,70 @@ def importwindow(master, widthfactor, heightfactor):
     HORFrame.grid(row=3, column=0, sticky='nswe', padx=10)
 
     #ArFrame Inputs
-    ArFrame.grid_rowconfigure(0, minsize=10)
+    ArFrame.grid_rowconfigure(0, minsize=10, weight=0)
+    ArFrame.grid_rowconfigure(1, minsize=30, weight=0)
+    ArFrame.grid_rowconfigure(2, minsize=30, weight=1)
+    ArFrame.grid_rowconfigure(3, minsize=10, weight=0)
     ArFrame.grid_columnconfigure(0, minsize=10)
     ArFrame.grid_columnconfigure(17, minsize=10)
     ct.CTkLabel(master=ArFrame, text='Ar CV:', text_font=("Calibri", -18), width=1).grid(row=1, column=1, sticky=tk.W)
     ct.CTkLabel(master=ArFrame, text='Scantype:', text_font=("Calibri", -18), width=1).grid(row=2, column=1, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Scan:', text_font=("Calibri", -18), width=1).grid(row=2, column=3, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Sep:', text_font=("Calibri", -18), width=1).grid(row=2, column=5, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Header:', text_font=("Calibri", -18), width=1).grid(row=2, column=7, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Decimal:', text_font=("Calibri", -18), width=1).grid(row=2, column=9, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Skip:', text_font=("Calibri", -18), width=1).grid(row=2, column=11, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Pot Column:', text_font=("Calibri", -18), width=1).grid(row=2, column=13, sticky=tk.W)
-    ct.CTkLabel(master=ArFrame, text='Cur Column:', text_font=("Calibri", -18), width=1).grid(row=2, column=15, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Scan Rate:', text_font=("Calibri", -18), width=1).grid(row=2, column=3, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Scan:', text_font=("Calibri", -18), width=1).grid(row=2, column=5, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Sep:', text_font=("Calibri", -18), width=1).grid(row=2, column=7, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Header:', text_font=("Calibri", -18), width=1).grid(row=2, column=9, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Decimal:', text_font=("Calibri", -18), width=1).grid(row=2, column=11, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Skip:', text_font=("Calibri", -18), width=1).grid(row=2, column=13, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Pot Column:', text_font=("Calibri", -18), width=1).grid(row=2, column=15, sticky=tk.W)
+    ct.CTkLabel(master=ArFrame, text='Cur Column:', text_font=("Calibri", -18), width=1).grid(row=2, column=17, sticky=tk.W)
 
     Scans = ("Single", "Multiple")
     ArScan_var = ct.StringVar()
-    ArScan = ttk.Combobox(ArFrame, textvariable=ArScan_var)
-    ArScan['values'] = Scans
-    ArScan['state'] = 'readonly'
-    ArScan.config(width=8)
-    ArScan_var.set(Ar_config[0])
+    ArScan = ct.CTkOptionMenu(master=ArFrame, variable=ArScan_var, values=Scans)
+    ArScan.config(width=80, fg_color=('grey70', 'grey30'), button_color=('grey70', 'grey30'),button_hover_color=('grey70', 'grey30'))
+    ArScan.set(Ar_config[0])
     ArScan.grid(row=2, column=2, sticky=tk.W)
 
-    ArScanNumber = ct.CTkEntry(master=ArFrame, width= 30)
-    ArScanNumber.grid(row=2, column=4, sticky=tk.W)
-    ArScanNumber.insert(0, Ar_config[1])
+    ArScanRate= ct.CTkEntry(master=ArFrame, width=50)
+    ArScanRate.grid(row=2, column=4, sticky=tk.W)
+    ArScanRate.insert(0, Ar_config[1])
+
+    ArScanNumber = ct.CTkEntry(master=ArFrame, width=30)
+    ArScanNumber.grid(row=2, column=6, sticky=tk.W)
+    ArScanNumber.insert(0, Ar_config[2])
 
     Seperators = (";", "spaces", "tabs")
     ArSep_var = ct.StringVar()
-    ArSep = ttk.Combobox(ArFrame, textvariable=ArSep_var)
-    ArSep['values'] = Seperators
-    ArSep['state'] = 'readonly'
-    ArSep.config(width=5)
-    ArSep_var.set(Ar_config[2])
-    ArSep.grid(row=2, column=6, sticky=tk.W)
+    ArSep = ct.CTkOptionMenu(master=ArFrame, variable=ArSep_var, values=Seperators)
+    ArSep.config(width=50, fg_color=('grey70', 'grey30'), button_color=('grey70', 'grey30'),button_hover_color=('grey70', 'grey30'))
+    ArSep.set(Ar_config[3])
+    ArSep.grid(row=2, column=8, sticky=tk.W)
 
     Options = ('None', '0', '1', '2', '3', '4', '5', '6')
     ArOption_var = ct.StringVar()
-    #ArHead = ttk.Combobox(ArFrame, textvariable=ArOption_var)
-    #ArHead['values'] = Options
-    #ArOption_var.set(Ar_config[3])
-    #ArHead.config(width=8)
     ArHead = ct.CTkComboBox(master=ArFrame, variable=ArOption_var, values=Options)
-    ArHead.config(width=80, fg_color=('grey80', 'grey20'), button_color=('grey80', 'grey20'), button_hover_color=('grey70', 'grey30'))
-    ArHead.grid(row=2, column=8, sticky=tk.W)
+    ArHead.config(width=70, fg_color=('grey70', 'grey30'), button_color=('grey70', 'grey30'), button_hover_color=('grey70', 'grey30'))
+    ArHead.set(Ar_config[4])
+    ArHead.grid(row=2, column=10, sticky=tk.W)
 
     Decimals = ('.', ',')
     ArDecimal_var = ct.StringVar()
-    ArDecimal = ttk.Combobox(ArFrame, textvariable=ArDecimal_var)
-    ArDecimal['values'] = Decimals
-    ArDecimal['state'] = 'readonly'
-    ArDecimal.config(width=5)
-    ArDecimal.grid(row=2, column=10, sticky=tk.W)
-    ArDecimal_var.set(Ar_config[4])
+    ArDecimal = ct.CTkOptionMenu(master=ArFrame, variable=ArDecimal_var, values=Decimals)
+    ArDecimal.config(width=50, fg_color=('grey70', 'grey30'), button_color=('grey70', 'grey30'), button_hover_color=('grey70', 'grey30'))
+    ArDecimal.set(Ar_config[5])
+    ArDecimal.grid(row=2, column=12, sticky=tk.W)
 
     ArSkip = ct.CTkEntry(master=ArFrame, width=10)
-    ArSkip.grid(row=2, column=12, sticky=tk.W)
-    ArSkip.insert(0, Ar_config[5])
+    ArSkip.grid(row=2, column=14, sticky=tk.W)
+    ArSkip.insert(0, Ar_config[6])
 
     ArPot = ct.CTkEntry(master=ArFrame, width=30)
-    ArPot.grid(row=2, column=14, sticky=tk.W)
-    ArPot.insert(0, Ar_config[6])
+    ArPot.grid(row=2, column=16, sticky=tk.W)
+    ArPot.insert(0, Ar_config[7])
 
     ArCur = ct.CTkEntry(master=ArFrame, width=30)
-    ArCur.grid(row=2, column=16, sticky=tk.W)
-    ArCur.insert(0, Ar_config[7])
+    ArCur.grid(row=2, column=18, sticky=tk.W)
+    ArCur.insert(0, Ar_config[8])
 
     # COFrame Inputs
     COFrame.grid_rowconfigure(0, minsize=10)
@@ -159,7 +159,7 @@ def importwindow(master, widthfactor, heightfactor):
 
         SaveText = f'Import Configure File\n' \
                    f'Ar\n' \
-                   f'{ArScan.get()}\t{ArScanNumber.get()}\t{ArSep.get()}\t{ArHead.get()}\t{ArDecimal.get()}\t{ArSkip.get()}\t{ArPot.get()}\t{ArCur.get()}\n' \
+                   f'{ArScan.get()}\t{ArScanRate.get()}\t{ArScanNumber.get()}\t{ArSep.get()}\t{ArHead.get()}\t{ArDecimal.get()}\t{ArSkip.get()}\t{ArPot.get()}\t{ArCur.get()}\n' \
                    f'CO\n' \
                    f'{COSep.get()}\t{COHead.get()}\t{CODecimal.get()}\n'
 
